7f23f2fc501ab8160d3605ecb9d66bd5a2fdfc8a
MahdiSaadatiOrg <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-07-30 14:47:09 +0430

    Add files via upload

---
+++ Queue_using_stack/Implementation.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_using_stack
+{
+    public class Implementation
+    {
+        int[] array;
+        int[] array2;
+        int top;
+        int top2;
+
+        public Implementation(int size)
+        {
+            array = new int[size];
+            array2 = new int[size];
+            top = -1;
+            top2 = -1;
+        }
+
+        bool IsFull()
+        {
+            if (top == array.Length - 1)
+            {
+                Console.WriteLine("The queue is full");
+                return true;
+            }
+            return false;
+        }
+        bool IsEmpty()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("The queue is empty");
+                return true;
+            }
+            return false;
+        }
+
+        private void Push(int Newnumber)
+        {
+            if (IsFull())
+            {
+
+            }
+            else
+            {
+                ++top;
+                array[top] = Newnumber;
+            }
+        }
+
+        private void Pop()
+        {
+            if (IsEmpty())
+            {
+
+            }
+            else
+            {
+                for (int i = top; i > -1; i--)
+                {
+                    top2++;
+                    array2[top2] = array[i];
+                }
+                top2 -= 2;
+                for (int i = top2; i > -1; i--)
+                {
+                    top = 0;
+                    array[top] = array2[i];
+                    top++;
+                }
+                top--;
+            }
+        }
+        public void PrintMembers()
+        {
+            if (IsEmpty())
+            {
+                
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+                 
+            }
+        }
+
+        public void EnQueue(int NewNumber) 
+        {
+            Push(NewNumber);
+        }
+
+        public void DeQueue() 
+        {
+            Pop();
+        }
+
+    }
+}
---
+++ Queue_using_stack/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Queue_using_stack;
+
+Console.WriteLine("Hello, World!");
+
+
+Implementation a = new Implementation(3);
+
+
+a.EnQueue(1);
+a.EnQueue(2);
+a.EnQueue(3);
+a.EnQueue(4);
+Console.WriteLine("items");
+a.PrintMembers();
+Console.WriteLine("---------------");
+a.DeQueue();
+a.DeQueue();
+a.DeQueue();
+a.EnQueue(8);
+Console.WriteLine("items");
+a.PrintMembers();
+Console.WriteLine("----------------");
+a.DeQueue();
+a.DeQueue();
+a.DeQueue();
+a.DeQueue();
